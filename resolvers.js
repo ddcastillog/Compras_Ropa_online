9b606a0298f81ddc70b4d18e91d0095c6f42e68a
Diego Castillo <ddcastillog@utn.edu.ec>
2021-01-18 23:16:38 -0500

    ll

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,23 @@
 const { db } = require("./cnn")
 const RopaResolver = {
+    Query: {
+        async pizzas(root,{id_producto}){    
+            if(name===undefined)    
+                return await db.any("select*from producto")
+            else
+                return await db.any("select*from producto where id_producto=$1",[id_producto]) 
+            }
+    },Producto:{
+        genero(producto){
+            return db.any(`select from genero where id_genero=$1 `,[producto.id_genero])
+        },
+        tipo_producto(producto){
+            return db.any(`select from tipo_producto where id_tipo=$1 `,[producto.id_tipo])
+        },
+        talla(producto){
+            return db.any(`select from talla where id_talla=$1 `,[producto.id_talla])
+        }
+    }
 
 
 }
