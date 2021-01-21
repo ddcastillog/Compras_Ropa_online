032c91c1151beae42dc58054f77fb1155a4dd3a2
Diego Castillo <ddcastillog@utn.edu.ec>
2021-01-20 20:43:02 -0500

    jj

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
-const typeDefs=mergeTypeDefs(loadFilesSync(`${__dirname}/**/*.graphql`));
+const typeDefs = mergeTypeDefs(loadFilesSync(`${__dirname}/**/*.graphql`));
 import resolvers from "./resolver"
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-  });
-  server.start({port: 3100}, ({port}) => {
-      console.log('Server on port', port);
-    });
+  typeDefs,
+  resolvers
+});
+server.start({ port: 3100 }, ({ port }) => {
+  console.log('Server on port http://localhost:', port);
+});
